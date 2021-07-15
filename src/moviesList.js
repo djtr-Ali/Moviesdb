a8e2727acf4a5688825c2531f4216c148aae35fc
djtr-Ali <djtrimranali@gmail.com>
2021-07-15 19:51:51 +0530

    final version

--- src/moviesList.js
+++ src/moviesList.js
@@ -7,7 +7,7 @@ function MoviesList(){
   const movie = useSelector((state)=>state);
   console.log(movie);
   return (
-    <>    
+    <>     
        <div className="movies">
           {movie.movies.map(((mov,index)=>{
             return <Movies poster={mov.poster_path} title={mov.title} desc={mov.overview} movie_id={mov.id} fav={movie.fav[index]}/>
